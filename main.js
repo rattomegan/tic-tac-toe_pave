b27c2ece6fc1a27cfe1179056b834fa9d5c7d9f0
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-03-29 17:27:24 -0400

    refactor code and add reset button while my palms aren't shorting my keyboard

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let winningCombos = [
+const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -7,67 +7,61 @@ let winningCombos = [
   [2, 5, 8],
   [0, 4, 8], 
   [2, 4, 6]
-]
+];
 
-let board, turn, winner
+let board, player, winner;
 
 let boardEl = document.querySelector('.board');
-let sqEls = document.querySelectorAll('.square')
+let sqEls = document.querySelectorAll('.square');
+let textEl = document.querySelector('h2');
 
-
-boardEl.addEventListener('click', makeMove)
+boardEl.addEventListener('click', makeMove);
+document.querySelector('button').addEventListener('click', init);
 
 function makeMove(evt) {
+  // clear text element before next move
+  textEl.textContent = ''
+  // get index of square that's clicked
   let idx = evt.target.id
-  board[idx] = player;
-  console.log(idx)
-  board.forEach(function(sq, idx) {
-    // if 1 = X || -1 = O
-    if(board[idx] === 1) {
-      sqEls[idx].textContent = "X"
-    }
-    if(board[idx] === -1) {
-      sqEls[idx].textContent = "O"
-    }
-    if(board[idx] === 0) {
-      sqEls[idx].textContent = ""
-    }
-  })
-  winner = getWinner()
-  render()
+  if (!board[idx]) {
+    board[idx] = player;
+  } else {
+    textEl.textContent = 'Pick another square'
+  }
+  winner = getWinner();
+  render();
   player *= -1;
 }
 
 function render() {
-  if (winner === 1) {
-    return document.querySelector('h2').innerText = 'Player 1 Won!'
-  }
-  if (winner === -1) {
-    return document.querySelector('h2').innerText = 'Player 2 Won!'
-  }
-  if (winner === 'Tie') {
-    return document.querySelector('h2').innerText = 'Tie!'
-  }
+  // display token
+  board.forEach(function(sq, idx) {
+    if(board[idx] === 1) sqEls[idx].textContent = "X";
+    if(board[idx] === -1) sqEls[idx].textContent = "O";
+    if(board[idx] === 0) sqEls[idx].textContent = "";
+  });
+  // display winner
+  if (winner === 1) return textEl.textContent = 'Player 1 Won!';
+  if (winner === -1) return textEl.textContent = 'Player 2 Won!';
+  if (winner === 'Tie') return textEl.textContent = 'Tie!';
 }
+
 function getWinner() {
   for (let i = 0; i < winningCombos.length; i++) {
     if(Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]) === 3) {
       return winner = board[winningCombos[i][0]]
     }
-  }
+  };
   if(board.includes(0)) return null;
-  return "Tie"
-}
+  return "Tie";
+};
 
 function init() {
-  board = [
-    0, 0, 0, 
-    0, 0, 0, 
-    0, 0, 0
-  ];
+  textEl.textContent = ''
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   player = 1;
-}
+  winner = null;
+  render();
+};
 
 init();
-
-// write function to determine if there's a win
